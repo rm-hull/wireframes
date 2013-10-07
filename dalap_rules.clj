058cd07962c4ff7d8794f59f33bcc2eb448ad231
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-07 21:13:36 +0100

    Fixed typos ... already !?!

--- dalap_rules.clj
+++ dalap_rules.clj
@@ -1,6 +1,6 @@
 {
   ["src/wireframes/transform.clj" "cljs-src/wireframes/transform.cljs"] []
-  ["src/wireframes/shape.clj" "cljs-src/wireframes/shape.clj"] []
+  ["src/wireframes/shape.clj" "cljs-src/wireframes/shape.cljs"] []
   ["src/wireframes/renderer/vector.clj" "cljs-src/wireframes/renderer/vector.cljs"] []
 }
 
