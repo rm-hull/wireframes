4998b193cafa545787939a0b9fc1e09276cb6e92
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-09 09:53:27 +0100

    Fix project name & latest lein-cljsbuild plugin

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject rm-hull/wireframe "0.0.1"
+(defproject rm-hull/wireframes "0.0.1"
   :description "A lightweight 3D wireframe renderer for both Clojure and ClojureScript"
-  :url "https://github.com/rm_hull/wireframe"
+  :url "https://github.com/rm_hull/wireframes"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -9,7 +9,7 @@
                  [hiccup "1.0.4"]
                  [jayq "2.4.0"]
                  [rm-hull/monet "0.1.8"]]
-  :plugins [[lein-cljsbuild "0.3.0"]
+  :plugins [[lein-cljsbuild "0.3.3"]
             [com.birdseye-sw/lein-dalap "0.1.0"]]
   :hooks [leiningen.dalap leiningen.cljsbuild]
   :source-path "src"
