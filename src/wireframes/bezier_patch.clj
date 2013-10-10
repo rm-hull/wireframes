bb3d641e114770fd160ad8630d24cbe23e2dcaff
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-10 10:45:54 +0100

    Move face connectivity calculations into loader - not really specific to bezier calcs; Fix 'off-by-one' bug when calculating the surface points (in bezier-patch namespace)

--- src/wireframes/bezier_patch.clj
+++ src/wireframes/bezier_patch.clj
@@ -24,34 +24,13 @@
                   (partition 4 control-points))]
     (evaluate-bezier-curve v u-curve)))
 
-(defn compute-control-points [vertices patch]
-  (map vertices patch))
-
-(defn surface-points [control-points divisions]
-  (let [divisions (double divisions)]
+(defn surface-points [divisions vertices patch]
+  (let [control-points (map vertices patch)
+        divisions (double divisions)]
     (vec
-      (for [j (range divisions)
-            i (range divisions)]
+      (for [j (range (inc divisions))
+            i (range (inc divisions))]
         (evaluate-bezier-patch
           control-points
           (/ i divisions)
           (/ j divisions))))))
-
-(defn- calculate-destination-index [source-index [dir offset] i j divisions]
-  (cond
-    (and (= dir :north) (= j 0)) nil
-    (and (= dir :east)  (= i (dec divisions))) nil
-    (and (= dir :south) (= j (dec divisions))) nil
-    (and (= dir :west)  (= i 0)) nil
-    :else (+ source-index offset)))
-
-(defn face-connectivity [divisions]
-  (vec
-    (distinct
-      (for [j (range divisions)
-            i (range divisions)
-            dir {:north (- divisions) :east 1 :south divisions :west  -1}
-            :let [src  (+ i (* j divisions))
-                  dest (calculate-destination-index src dir i j divisions)]
-            :when dest]
-        (if (< src dest) [src dest] [dest src])))))
--- src/wireframes/shape_loader.clj
+++ src/wireframes/shape_loader.clj
@@ -14,27 +14,50 @@
     (str/split line #",")
     (map converter)))
 
-(defn- create-points [vertices-data]
+(defn- create-vertices [vertices-data]
   (mapv (comp vec (partial parse-csv parse-double)) vertices-data))
 
 (defn create-patches [patch-data]
-  (let [f (partial parse-csv (comp dec parse-int))] ; teapot indexes start at 1...
-    (->>                                            ; decrement for zero-offset indexing
-      patch-data
-      (map f))))
+  ; teapot indexes start at 1... decrement for zero-offset indexing
+  (let [f (partial parse-csv (comp dec parse-int))]
+    (mapv f patch-data)))
+
+(defn- calculate-destination-index [source-index [dir offset] i j divisions]
+  (cond
+    (and (= dir :north) (= j 0)) nil
+    (and (= dir :east)  (= i (dec divisions))) nil
+    (and (= dir :south) (= j (dec divisions))) nil
+    (and (= dir :west)  (= i 0)) nil
+    :else (+ source-index offset)))
+
+(defn face-connectivity [divisions]
+  (vec
+    (distinct
+      (for [j (range divisions)
+            i (range divisions)
+            dir {
+                 ;:north (- divisions)
+                 :east 1
+                 :south divisions
+                 ;:west  -1
+                 }
+            :let [src  (+ i (* j divisions))
+                  dest (calculate-destination-index src dir i j divisions)]
+            :when dest]
+        (if (< src dest) [src dest] [dest src])))))
 
 (defn- create-surface [divisions vertices patch]
-  (let [control-points (bp/compute-control-points vertices patch)]
-    {:points (bp/surface-points control-points divisions)
-     :lines  (bp/face-connectivity divisions)
-     :polygons nil }))
+  {:points (bp/surface-points divisions vertices patch)
+   :lines  (face-connectivity (inc divisions))
+   :polygons nil })
 
 (defn load-shape [file divisions]
   (let [raw-data    (vec (str/split-lines (slurp file)))
         num-patches (Integer/parseInt (raw-data 0))
         patches     (create-patches (subvec raw-data 1 (inc num-patches)))
-        vertices    (create-points (subvec raw-data (+ num-patches 2)))]
+        vertices    (create-vertices (subvec raw-data (+ num-patches 2)))]
       (->>
+        ;(take 2 (drop 5 patches))
         patches
         (map (partial create-surface divisions vertices))
         (reduce sp/augment)
