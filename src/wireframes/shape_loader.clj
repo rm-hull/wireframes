e883e3d79307d7b9bf1707d67ad8bf256b80c530
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-10 19:11:29 +0100

    Better polygon construction & tidy face connectivity

--- src/wireframes/shape_loader.clj
+++ src/wireframes/shape_loader.clj
@@ -1,5 +1,6 @@
 (ns wireframes.shape-loader
   (:require [clojure.string :as str]
+            [wireframes.transform :as t]
             [wireframes.shape-primitives :as sp]
             [wireframes.bezier-patch :as bp]))
 
@@ -22,46 +23,51 @@
   (let [f (partial parse-csv (comp dec parse-int))]
     (mapv f patch-data)))
 
-(defn- calculate-destination-index [source-index [dir offset] i j divisions]
+(defn- calculate-destination-index [source-index [dir offset] i j num-points]
   (cond
-    (and (= dir :north) (= j 0)) nil
-    (and (= dir :east)  (= i (dec divisions))) nil
-    (and (= dir :south) (= j (dec divisions))) nil
-    (and (= dir :west)  (= i 0)) nil
+    (and (= dir :east)  (= i (dec num-points))) nil
+    (and (= dir :south) (= j (dec num-points))) nil
     :else (+ source-index offset)))
 
-(defn face-connectivity [divisions]
+(defn face-connectivity [num-points]
   (vec
     (distinct
+      (for [j (range num-points)
+            i (range num-points)
+            dir { :east 1 :south num-points }
+            :let [src  (+ i (* j num-points))
+                  dest (calculate-destination-index src dir i j num-points)]
+            :when dest]
+        [src dest]))))
+
+(defn polygons [divisions]
+  (vec
+    (apply concat
       (for [j (range divisions)
             i (range divisions)
-            dir {
-                 ;:north (- divisions)
-                 :east 1
-                 :south divisions
-                 ;:west  -1
-                 }
-            :let [src  (+ i (* j divisions))
-                  dest (calculate-destination-index src dir i j divisions)]
-            :when dest]
-        (if (< src dest) [src dest] [dest src])))))
+            :let [
+                  a (+ i (* j (inc divisions)))
+                  b (inc a)
+                  c (+ b divisions)
+                  d (inc c)]]
+        (t/triangulate [a b d c]))))) ; order of points is important
 
 (defn- create-surface [divisions vertices patch]
   {:points (bp/surface-points divisions vertices patch)
    :lines  (face-connectivity (inc divisions))
-   :polygons nil })
+   :polygons (polygons divisions)})
 
 (defn load-shape [file divisions]
   (let [raw-data    (vec (str/split-lines (slurp file)))
         num-patches (Integer/parseInt (raw-data 0))
-        patches     (create-patches (subvec raw-data 1 (inc num-patches)))
-        vertices    (create-vertices (subvec raw-data (+ num-patches 2)))]
+	patches     (create-patches (subvec raw-data 1 (inc num-patches)))
+	vertices    (create-vertices (subvec raw-data (+ num-patches 2)))]
       (->>
-        ;(take 2 (drop 5 patches))
-        patches
-        (map (partial create-surface divisions vertices))
-        (reduce sp/augment)
-        ; TODO: create polygons
+	;(take 2 (drop 5 patches))
+	patches
+	(map (partial create-surface divisions vertices))
+	(reduce sp/augment)
+	; TODO: create polygons
         )))
 
 (comment
