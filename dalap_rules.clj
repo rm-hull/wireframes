56282b2a6e10fd5503bfd2b786efc8daf94db680
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-11 11:12:15 +0100

    Another cljs rule

--- dalap_rules.clj
+++ dalap_rules.clj
@@ -3,6 +3,7 @@
   ["src/wireframes/shape_primitives.clj" "target/generated/wireframes/shape_primitives.cljs"] []
   ["src/wireframes/shape_loader.clj" "target/generated/wireframes/shape_loader.cljs"] []
   ["src/wireframes/bezier_patch.clj" "target/generated/wireframes/bezier_patch.cljs"] []
+  ["src/wireframes/renderer.clj" "target/generated/wireframes/renderer.cljs"] []
   ["src/wireframes/renderer/vector.clj" "target/generated/wireframes/renderer/vector.cljs"] []
   ["src/wireframes/renderer/canvas.cljs" "target/generated/wireframes/renderer/canvas.cljs"] []
 }
