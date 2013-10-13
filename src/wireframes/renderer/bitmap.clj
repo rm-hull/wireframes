fcb876e7485a659cf860d954a00ac6ef8d6142e8
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-13 18:00:17 +0100

    Move examples out of bitmap comments into own examples file Draw and fill solid shapes

--- src/wireframes/renderer/bitmap.clj
+++ src/wireframes/renderer/bitmap.clj
@@ -1,11 +1,5 @@
 (ns wireframes.renderer.bitmap
   (:use [wireframes.renderer :only [get-3d-points get-2d-points priority-fill calculate-illumination]])
-  (:require [wireframes.transform :as t]
-            [wireframes.shapes.primitives :as p]
-            [wireframes.shapes.patch-loader :as pl]
-            [wireframes.shapes.wavefront-loader :as wl]
-            [wireframes.shapes.platonic-solids :as ps]
-            [wireframes.shapes.curved-solids :as cs])
   (:import [java.awt.image BufferedImage]
            [java.awt.geom AffineTransform GeneralPath Ellipse2D$Double]
            [java.awt Color Graphics2D RenderingHints BasicStroke GraphicsEnvironment]
@@ -35,7 +29,7 @@
   (.closePath path)
   path)
 
-(defn draw-wireframe [^Graphics2D g2d focal-length transform shape]
+(defn draw-wireframe [focal-length transform shape ^Graphics2D g2d]
   (let [path (GeneralPath.)
         points-3d (get-3d-points transform shape)
         points-2d (get-2d-points focal-length points-3d)]
@@ -58,7 +52,7 @@
       (apply add-line path (map points-2d line)))
     (.draw g2d path)))
 
-(defn draw-solid [^Graphics2D g2d focal-length transform shape & [alpha]]
+(defn draw-solid [focal-length transform shape ^Graphics2D g2d]
   (let [path (GeneralPath.)
         points-3d (get-3d-points transform shape)
         points-2d (get-2d-points focal-length points-3d)
@@ -70,7 +64,10 @@
         (calculate-illumination)
         (create-color)
         (.setColor g2d))
-      (.fill g2d (create-polygon path (map points-2d polygon))))))
+      (let [p (create-polygon path (map points-2d polygon))]
+        (doto g2d
+          (.fill p)
+          (.draw p))))))
 
 (defn create-image [w h]
   (if (GraphicsEnvironment/isHeadless)
@@ -81,7 +78,7 @@
            (GraphicsEnvironment/getLocalGraphicsEnvironment)))
        w h)))
 
-(defn ->img [focal-length transform shape [w h]]
+(defn ->img [draw-fn [w h]]
   (let [img (create-image w h)
         g2d (.createGraphics img)
         scale (min (quot w 2) (quot h 2))]
@@ -95,109 +92,9 @@
       (.setRenderingHint RenderingHints/KEY_STROKE_CONTROL RenderingHints/VALUE_STROKE_NORMALIZE)
       (.setRenderingHint RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
       (.setRenderingHint RenderingHints/KEY_RENDERING RenderingHints/VALUE_RENDER_QUALITY))
-    (draw-wireframe g2d focal-length transform shape)
-    ;(draw-solid g2d focal-length transform shape)
+    (draw-fn g2d)
     (.dispose g2d)
     img))
 
 (defn write-png [image filename]
   (ImageIO/write image "png" (clojure.java.io/file filename)))
-
-(comment
-
-  (time
-    (write-png
-      (->img
-        10
-        (t/concat
-          (t/rotate :z (p/degrees->radians 35))
-          (t/rotate :x (p/degrees->radians -120))
-          (t/translate 0 -1 40))
-        (pl/load-shape "resources/newell-teapot/teapot" 16)
-        [1000 900])
-      "doc/gallery/solid-teapot.png"))
-
-  (time
-    (write-png
-      (->img
-        4
-        (t/concat
-          (t/rotate :z (p/degrees->radians 25))
-          (t/rotate :y (p/degrees->radians 130))
-          (t/rotate :x (p/degrees->radians -50))
-          (t/translate 3 -5 60))
-        (wl/load-shape "resources/obj_IconA5.obj")
-        [1000 900])
-      "doc/gallery/wireframe-icon-a5.png"))
-
-  (time
-    (write-png
-      (->img
-        4
-        (t/concat
-          (t/rotate :z (p/degrees->radians 25))
-          (t/rotate :y (p/degrees->radians 130))
-          (t/rotate :x (p/degrees->radians -50))
-          (t/translate 3 -5 60))
-        (wl/load-shape "resources/obj_IconA5.obj")
-        [1000 900])
-      "doc/gallery/solid-icon-a5.png"))
-
-  (write-png
-    (->img
-      10
-      (t/concat
-        (t/rotate :x (p/degrees->radians -30))
-        (t/translate 0 0 25))
-      (pl/load-shape "resources/newell-teapot/teacup" 16)
-      [1000 900])
-    "doc/gallery/solid-teacup.png")
-
-  (write-png
-    (->img
-      3
-      (t/concat
-        (t/rotate :z (p/degrees->radians 65))
-        (t/rotate :y (p/degrees->radians -30))
-        (t/translate 0 0 16))
-      (cs/make-torus 1 3 60 60)
-      [400 400])
-    "doc/gallery/wireframe-torus.png")
-
-  (write-png
-    (->img
-      3
-      (t/concat
-        (t/rotate :z (p/degrees->radians 65))
-        (t/rotate :y (p/degrees->radians -30))
-        (t/translate 0 0 16))
-      ps/tetrahedron
-      [400 400])
-    "tetrahedron.png")
-
-
-  (write-png
-    (->img
-      3
-      (t/concat
-        ;(t/rotate :z (p/degrees->radians 65))
-        (t/rotate :x (p/degrees->radians 60))
-        (t/rotate :y (p/degrees->radians -15))
-        (t/translate 0 0 16))
-      (cs/make-sphere 3 30)
-      [900 900])
-    "doc/gallery/wireframe-sphere.png")
-
-
-  (write-png
-    (->img
-      8
-      (t/concat
-        (t/rotate :z (p/degrees->radians 15))
-        (t/rotate :x (p/degrees->radians  -20))
-        (t/translate 0 -0.2 5.5))
-      (cs/make-wineglass 60)
-      [900 900])
-    "doc/gallery/wireframe-wineglass.png")
-)
-
---
+++ test/wireframes/examples.clj
@@ -0,0 +1,127 @@
+(ns wireframes.examples
+  (:use [clojure.test])
+  (:require [wireframes.transform :as t]
+            [wireframes.shapes.primitives :as p]
+            [wireframes.shapes.curved-solids :as cs]
+            [wireframes.shapes.platonic-solids :as ps]
+            [wireframes.shapes.patch-loader :as pl]
+            [wireframes.shapes.wavefront-loader :as wl]
+            [wireframes.renderer.bitmap :as b]))
+
+(defn harness [{:keys [shape focal-length transform draw-fn filename size]}]
+  (println filename)
+  (time
+    (b/write-png
+      (b/->img
+        (partial draw-fn focal-length transform shape)
+        (or size [1000 900]))
+      filename)))
+
+(deftest ^:examples wireframes
+
+  (harness {
+    :filename "doc/gallery/wireframe-teapot.png"
+    :shape (pl/load-shape "resources/newell-teapot/teapot" 16)
+    :focal-length 10
+    :draw-fn b/draw-wireframe
+    :transform (t/concat
+                 (t/rotate :z (p/degrees->radians 35))
+                 (t/rotate :x (p/degrees->radians -120))
+                 (t/translate 0 -1 40))})
+
+  (harness {
+    :filename "doc/gallery/wireframe-icon-a5.png"
+    :shape (wl/load-shape "resources/obj_IconA5.obj")
+    :focal-length 4
+    :draw-fn b/draw-wireframe
+    :transform (t/concat
+                 (t/rotate :z (p/degrees->radians 25))
+                 (t/rotate :y (p/degrees->radians 130))
+                 (t/rotate :x (p/degrees->radians -50))
+                 (t/translate 3 -5 60))})
+
+  (harness {
+    :filename "doc/gallery/wireframe-torus.png"
+    :shape (cs/make-torus 1 3 60 60)
+    :focal-length 3
+    :size [400 400]
+    :draw-fn b/draw-wireframe
+    :transform (t/concat
+                 (t/rotate :z (p/degrees->radians 65))
+                 (t/rotate :y (p/degrees->radians -30))
+                 (t/translate 0 0 16))})
+
+  (harness {
+    :filename "doc/gallery/wireframe-sphere.png"
+    :shape (cs/make-sphere 3 30)
+    :focal-length 3
+    :draw-fn b/draw-wireframe
+    :transform (t/concat
+                 (t/rotate :x (p/degrees->radians 60))
+                 (t/rotate :y (p/degrees->radians -15))
+                 (t/translate 0 0 16))})
+
+  (harness {
+    :filename "doc/gallery/wireframe-wineglass.png"
+    :shape (cs/make-wineglass 60)
+    :focal-length 8
+    :draw-fn b/draw-wireframe
+    :transform (t/concat
+                 (t/rotate :z (p/degrees->radians 15))
+                 (t/rotate :x (p/degrees->radians -20))
+                 (t/translate 0 -0.2 5.5))})
+
+  (harness {
+    :filename "doc/gallery/tetrahedron.png"
+    :shape ps/tetrahedron
+    :focal-length 5
+    :size [300 300]
+    :draw-fn b/draw-wireframe
+    :transform (t/concat
+                 (t/rotate :z (p/degrees->radians 55))
+                 (t/rotate :y (p/degrees->radians -20))
+                 (t/translate 0 0 16))})
+
+  (harness {
+    :filename "doc/gallery/icosahedron.png"
+    :shape ps/icosahedron
+    :focal-length 5
+    :size [300 300]
+    :draw-fn b/draw-wireframe
+    :transform (t/concat
+                 (t/rotate :z (p/degrees->radians 55))
+                 (t/rotate :y (p/degrees->radians -30))
+                 (t/translate 0 0 16))}))
+
+
+(deftest ^:examples solids
+  (harness {
+    :filename "doc/gallery/solid-teapot.png"
+    :shape (pl/load-shape "resources/newell-teapot/teapot" 16)
+    :focal-length 10
+    :draw-fn b/draw-solid
+    :transform (t/concat
+                 (t/rotate :z (p/degrees->radians 35))
+                 (t/rotate :x (p/degrees->radians -120))
+                 (t/translate 0 -1 40))})
+
+  (harness {
+    :filename "doc/gallery/solid-icon-a5.png"
+    :shape (wl/load-shape "resources/obj_IconA5.obj")
+    :focal-length 4
+    :draw-fn b/draw-solid
+    :transform (t/concat
+                 (t/rotate :z (p/degrees->radians 25))
+                 (t/rotate :y (p/degrees->radians 130))
+                 (t/rotate :x (p/degrees->radians -50))
+                 (t/translate 3 -5 60))})
+
+  (harness {
+    :filename "doc/gallery/solid-teacup.png"
+    :shape (pl/load-shape "resources/newell-teapot/teacup" 16)
+    :focal-length 10
+    :draw-fn b/draw-solid
+    :transform (t/concat
+                 (t/rotate :x (p/degrees->radians -30))
+                 (t/translate 0 0 25))}))
+
