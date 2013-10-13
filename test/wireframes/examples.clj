891b615b9e135ea9988444adf2d89cc748080ec7
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-13 21:44:27 +0100

    Reworked examples

--- test/wireframes/examples.clj
+++ test/wireframes/examples.clj
@@ -6,42 +6,39 @@
             [wireframes.shapes.platonic-solids :as ps]
             [wireframes.shapes.patch-loader :as pl]
             [wireframes.shapes.wavefront-loader :as wl]
-            [wireframes.renderer.bitmap :as b]))
+            [wireframes.renderer.bitmap :as b])
+  (:import [java.awt Color]))
 
-(defn harness [{:keys [shape focal-length transform draw-fn filename size]}]
-  (println filename)
-  (time
-    (is (b/write-png
-        (b/->img
-          (partial draw-fn focal-length transform shape)
-          (or size [1000 900]))
-        filename))))
+(defn create-color [style & [^Color color]]
+  (let [color (or color Color/WHITE)
+        alpha (style {:transparent 0 :translucent 128 :opaque 255})]
+    (when alpha
+      (Color.
+        (.getRed color)
+        (.getGreen color)
+        (.getBlue color)
+        (int alpha)))))
 
-(deftest ^:examples wireframes
+(defn harness [{:keys [shape focal-length transform
+                       draw-fn filename size style color] :as opts}]
+  (let [dir (str "doc/gallery/" (name style) "/")]
+    (.mkdir (clojure.java.io/file dir))
+    (println filename)
+    (time
+      (is
+        (b/write-png
+          (b/->img
+            (partial
+              (or draw-fn b/draw-solid)
+              (assoc opts :fill-color (create-color style color)))
+            (or size [1000 900]))
+          (str dir filename))))))
 
-  (harness {
-    :filename "doc/gallery/wireframe-teapot.png"
-    :shape (pl/load-shape "resources/newell-teapot/teapot" 16)
-    :focal-length 10
-    :draw-fn b/draw-wireframe
-    :transform (t/concat
-                 (t/rotate :z (p/degrees->radians 35))
-                 (t/rotate :x (p/degrees->radians -120))
-                 (t/translate 0 -1 40))})
-
-  (harness {
-    :filename "doc/gallery/wireframe-icon-a5.png"
-    :shape (wl/load-shape "resources/obj_IconA5.obj")
-    :focal-length 4
-    :draw-fn b/draw-wireframe
-    :transform (t/concat
-                 (t/rotate :z (p/degrees->radians 25))
-                 (t/rotate :y (p/degrees->radians 130))
-                 (t/rotate :x (p/degrees->radians -50))
-                 (t/translate 3 -5 60))})
+(deftest ^:examples simple-shapes
 
   (harness {
-    :filename "doc/gallery/wireframe-torus.png"
+    :filename "torus.png"
+    :style :transparent
     :shape (cs/make-torus 1 3 60 60)
     :focal-length 3
     :size [400 400]
@@ -52,7 +49,8 @@
                  (t/translate 0 0 16))})
 
   (harness {
-    :filename "doc/gallery/wireframe-sphere.png"
+    :filename "sphere.png"
+    :style :transparent
     :shape (cs/make-sphere 3 30)
     :focal-length 3
     :draw-fn b/draw-wireframe
@@ -62,7 +60,8 @@
                  (t/translate 0 0 16))})
 
   (harness {
-    :filename "doc/gallery/wireframe-wineglass.png"
+    :filename "wineglass.png"
+    :style :transparent
     :shape (cs/make-wineglass 60)
     :focal-length 8
     :draw-fn b/draw-wireframe
@@ -72,7 +71,8 @@
                  (t/translate 0 -0.2 5.5))})
 
   (harness {
-    :filename "doc/gallery/tetrahedron.png"
+    :filename "tetrahedron.png"
+    :style :transparent
     :shape ps/tetrahedron
     :focal-length 5
     :size [300 300]
@@ -83,7 +83,8 @@
                  (t/translate 0 0 16))})
 
   (harness {
-    :filename "doc/gallery/icosahedron.png"
+    :filename "icosahedron.png"
+    :style :transparent
     :shape ps/icosahedron
     :focal-length 5
     :size [300 300]
@@ -93,35 +94,39 @@
                  (t/rotate :y (p/degrees->radians -30))
                  (t/translate 0 0 16))}))
 
+(deftest ^:examples complex-solids
+  (let [teapot (pl/load-shape "resources/newell-teapot/teapot" 16)
+        teacup (pl/load-shape "resources/newell-teapot/teacup" 16)
+        plane (wl/load-shape "resources/obj_IconA5.obj")]
+    (doseq [style [:transparent :translucent :opaque :shaded]]
+      (harness {
+        :filename "teapot.png"
+        :style style
+        :shape teapot
+        :focal-length 10
+        :transform (t/concat
+                     (t/rotate :z (p/degrees->radians 35))
+                     (t/rotate :x (p/degrees->radians -120))
+                     (t/translate 0 -1 40))})
 
-(deftest ^:examples solids
-  (harness {
-    :filename "doc/gallery/solid-teapot.png"
-    :shape (pl/load-shape "resources/newell-teapot/teapot" 16)
-    :focal-length 10
-    :draw-fn b/draw-solid
-    :transform (t/concat
-                 (t/rotate :z (p/degrees->radians 35))
-                 (t/rotate :x (p/degrees->radians -120))
-                 (t/translate 0 -1 40))})
+      (harness {
+        :filename "teacup.png"
+        :style style
+        :shape teacup
+        :focal-length 10
+        :transform (t/concat
+                     (t/rotate :x (p/degrees->radians -30))
+                     (t/translate 0 0 25))})
 
-  (harness {
-    :filename "doc/gallery/solid-icon-a5.png"
-    :shape (wl/load-shape "resources/obj_IconA5.obj")
-    :focal-length 4
-    :draw-fn b/draw-solid
-    :transform (t/concat
-                 (t/rotate :z (p/degrees->radians 25))
-                 (t/rotate :y (p/degrees->radians 130))
-                 (t/rotate :x (p/degrees->radians -50))
-                 (t/translate 3 -5 60))})
-
-  (harness {
-    :filename "doc/gallery/solid-teacup.png"
-    :shape (pl/load-shape "resources/newell-teapot/teacup" 16)
-    :focal-length 10
-    :draw-fn b/draw-solid
-    :transform (t/concat
-                 (t/rotate :x (p/degrees->radians -30))
-                 (t/translate 0 0 25))}))
+      (harness {
+        :filename "icon-a5.png"
+        :style style
+        :shape plane
+        :focal-length 4
+        :transform (t/concat
+                     (t/rotate :z (p/degrees->radians 25))
+                     (t/rotate :y (p/degrees->radians 130))
+                     (t/rotate :x (p/degrees->radians -50))
+                     (t/translate 3 -5 60))})
+      )))
 
