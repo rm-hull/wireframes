5870964a5bad01d2f164822769deb3f8b05298d2
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-14 21:01:09 +0100

    Rework solids in terms of polygons rather than lines

--- src/wireframes/shapes/platonic_solids.clj
+++ src/wireframes/shapes/platonic_solids.clj
@@ -13,8 +13,7 @@
             [-1.0  0.0 (/ -1 √2)]
             [ 0.0  1.0 (/  1 √2)]
             [ 0.0 -1.0 (/  1 √2)]]
-   :lines  [[0 1] [1 2] [2 0] [1 3] [2 3] [0 3]]
-   :polgons nil})
+   :polygons [[0 1 2] [0 2 3] [0 1 3] [1 2 3]]})
 
 (def cube
   "Start with a point, extrude to a line alone the Z-plane, then extrude that
@@ -36,14 +35,17 @@
                  (apply concat
                    (for [one [-1 1] rho [φ -φ]]
                      [[0 one rho] [one rho 0] [rho 0 one]])))
-        lines (vec
+        polygons (vec
                 (for [a (range (count points))
-                      b (range (count points))
-                      :when (and (not= a b) (= (t/distance (points a) (points b)) 2.0))]
-                  [a b]))]
+                      b (range a)
+                      c (range b)
+                      :when (and
+                              (= (t/distance (points a) (points b)) 2.0)
+                              (= (t/distance (points a) (points c)) 2.0)
+                              (= (t/distance (points b) (points c)) 2.0))]
+                  [a b c]))]
   {:points points
-   :lines lines
-   :polygons nil }))
+   :polygons polygons }))
 
 
 
