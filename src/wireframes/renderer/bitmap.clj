56b87c7accf16c630a124ac57c1a9223aa25b2cf
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-18 11:24:36 +0100

    Type hinting and some refactoring

--- src/wireframes/renderer/bitmap.clj
+++ src/wireframes/renderer/bitmap.clj
@@ -13,67 +13,57 @@
       (let [brightness (Math/max 5 (Math/min brightness 250))]
         (Color. brightness brightness brightness)))))
 
-(defn- draw-dot [^Graphics2D g2d [^double x ^double y] size]
-  (.fill g2d (Ellipse2D$Double. (- x (/ size 2)) (- y (/ size 2)) size size)))
+(defn transparent? [^Color color]
+  (and color (zero? (.getAlpha color))))
 
-(defn- add-line [^GeneralPath path [^double ax ^double ay] [^double bx ^double by]]
-  (doto path
-    (.moveTo ax ay)
-    (.lineTo bx by))
-  path)
+(defn solid? [^Color color]
+  (and color (pos? (.getAlpha color))))
 
-(defn- create-polygon [^GeneralPath path [[^double ax ^double ay] & more]]
-  (doto path
-    (.reset)
-    (.moveTo ax ay))
-  (doseq [[^double bx ^double by] more]
-    (.lineTo path bx by))
-  (.closePath path)
-  path)
+(defn walk-polygon [^GeneralPath path points-2d polygon]
+  (let [[^double ax ^double ay] (get points-2d (first polygon))]
+    (doto path
+      (.reset)
+      (.moveTo ax ay))
+    (loop [ps (next polygon)]
+      (when-let [[^double bx ^double by] (get points-2d (first ps))]
+        (.lineTo path bx by)
+        (recur (next ps))))
+    (.closePath path)))
 
-(defn draw-wireframe [{:keys [focal-length transform shape]} ^Graphics2D g2d]
-  (let [path (GeneralPath.)
-        points-3d (get-3d-points transform shape)
-        points-2d (get-2d-points focal-length points-3d)]
+(defn wireframe-draw-fn [^Graphics2D g2d points-2d ^Color fill-color ^Color edge-color]
+  (let [path (GeneralPath.)]
+    (fn [polygon]
+      (walk-polygon path points-2d polygon)
+      (doto g2d
+        (.setColor fill-color)
+        (.fill path)
+        (.setColor edge-color)
+        (.draw path)))))
 
-    ;(.setColor g2d Color/RED)
-    ;(doseq [[idx1 idx2] (:lines shape)]
-    ;  (draw-dot g2d (points-2d idx1) 0.008))
-
-    ;(.setColor g2d Color/GREEN)
-    ;(doseq [[idx1 idx2 idx3] (:polygons shape)]
-    ;  (add-line path (points-2d idx1) (points-2d idx2))
-    ;  (add-line path (points-2d idx1) (points-2d idx3))
-    ;  (add-line path (points-2d idx2) (points-2d idx3))
-    ;  )
-    ;(.draw g2d path)
-    ;(.reset path)
-
-    (.setColor g2d Color/BLACK)
-    (doseq [line (:lines shape)]
-      (apply add-line path (map points-2d line)))
-    (.draw g2d path)))
+(defn shader-draw-fn [^Graphics2D g2d points-2d shader]
+  (let [path (GeneralPath.)]
+    (fn [polygon]
+      (walk-polygon path points-2d polygon)
+      (doto g2d
+        (.setColor (shader polygon))
+        (.fill path)
+        (.draw path)))))
 
 (defn draw-solid [{:keys [focal-length transform shape fill-color]} ^Graphics2D g2d]
-  (let [path (GeneralPath.)
+  (let [
         points-3d (get-3d-points transform shape)
         points-2d (get-2d-points focal-length points-3d)
-        shader    (shader points-3d create-color)
         polygons  (cond
-                    (and fill-color (zero? (.getAlpha fill-color))) (:polygons shape)
-                    (and fill-color) (sort-by (priority-fill points-3d) (:polygons shape))
-                    :else (sort-by (priority-fill points-3d) (t/reduce-polygons (:polygons shape))))]
-    (doseq [polygon polygons
-            :let [p (create-polygon path (map points-2d polygon))
-                  face-color (if fill-color fill-color (shader polygon))
-                  edge-color (if fill-color Color/BLACK face-color)]]
-        (doto g2d
-          (.setColor face-color)
-          (.fill p)
-          (.setColor edge-color)
-          (.draw p)))))
+                    (transparent? fill-color) (:polygons shape)
+                    (solid? fill-color)       (sort-by (priority-fill points-3d) (:polygons shape))
+                    :else                     (sort-by (priority-fill points-3d) (t/reduce-polygons (:polygons shape))))
+        draw-fn   (if (or (transparent? fill-color) (solid? fill-color))
+                    (wireframe-draw-fn g2d points-2d fill-color Color/BLACK)
+                    (shader-draw-fn g2d points-2d (shader points-3d create-color)))]
+    (doseq [polygon polygons]
+      (draw-fn polygon))))
 
-(defn create-image [w h]
+(defn ^BufferedImage create-image [w h]
   (if (GraphicsEnvironment/isHeadless)
     (BufferedImage. w h BufferedImage/TYPE_INT_ARGB)
     (.createCompatibleImage
@@ -82,23 +72,28 @@
            (GraphicsEnvironment/getLocalGraphicsEnvironment)))
        w h)))
 
+(defn ^Graphics2D create-graphics [^BufferedImage img]
+  (let [g2d (.createGraphics img)]
+    (doto g2d
+      (.setRenderingHint RenderingHints/KEY_STROKE_CONTROL RenderingHints/VALUE_STROKE_NORMALIZE)
+      (.setRenderingHint RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
+      (.setRenderingHint RenderingHints/KEY_RENDERING RenderingHints/VALUE_RENDER_QUALITY))
+    g2d))
+
 (defn ->img [draw-fn [w h]]
   (let [img (create-image w h)
-        g2d (.createGraphics img)
-        scale (min (quot w 2) (quot h 2))]
+        g2d (create-graphics img)
+        scale (double (min (/ w 2) (/ h 2)))]
     (doto g2d
       (.setBackground Color/WHITE)
       (.clearRect 0 0 w h)
       (.setColor Color/BLACK)
-      (.translate (quot w 2) (quot h 2))
+      (.translate (double (/ w 2)) (double (/ h 2)))
       (.scale scale scale)
-      (.setStroke (BasicStroke. (/ 1.0 (quot w 2))))
-      (.setRenderingHint RenderingHints/KEY_STROKE_CONTROL RenderingHints/VALUE_STROKE_NORMALIZE)
-      (.setRenderingHint RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
-      (.setRenderingHint RenderingHints/KEY_RENDERING RenderingHints/VALUE_RENDER_QUALITY))
+      (.setStroke (BasicStroke. (/ 0.5 w))))
     (draw-fn g2d)
     (.dispose g2d)
     img))
 
-(defn write-png [image filename]
+(defn write-png [^BufferedImage image filename]
   (ImageIO/write image "png" (clojure.java.io/file filename)))
