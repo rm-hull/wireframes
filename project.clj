52e1500b49fa9a24c2a6d3f1675e0b186c6b2733
Richard Hull <rm_hull@yahoo.co.uk>
2013-11-18 21:21:53 +0000

    Dependency updates

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1934"]
+                 [org.clojure/clojurescript "0.0-2030"]
                  [org.clojure/core.rrb-vector "0.0.10-SNAPSHOT"]
                  [gloss "0.2.2"]
-                 [potemkin "0.3.3"]
-                 [byte-streams "0.1.6-SNAPSHOT"]
-                 [com.taoensso/timbre "2.6.3"]
+                 [potemkin "0.3.4"]
+                 [byte-streams "0.1.6"]
+                 [com.taoensso/timbre "2.7.1"]
                  [prismatic/dommy "0.1.2"]
                  [hiccup "1.0.4"]
-                 [jayq "2.4.0"]
+                 [jayq "2.5.0"]
                  [rm-hull/monet "0.1.9"]]
   :plugins [[lein-cljsbuild "0.3.4"]
             [com.birdseye-sw/lein-dalap "0.1.0"]]
@@ -31,8 +31,8 @@
           :output-to "target/wireframes.js"
           :source-map "target/wireframes.map"
           :static-fns true
-          :optimizations :advanced
-          :pretty-print false
+          ;:optimizations :advanced
+          :pretty-print true
           :externs ["resources/private/externs/jquery.js"] }}}}
   :test-selectors {:default (complement :examples)
                    :examples :examples }
